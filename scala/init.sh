3aebb7b393ccd00d4e88c9c8c41a4ec77f5786a7
Phil Messenger <phil@Phils-MacBook-Pro.local>
2015-09-23 10:21:15 +0100

    Upgrade to Scala 2.11

--- scala/init.sh
+++ scala/init.sh
@@ -7,14 +7,14 @@ if [ -d "scala" ]; then
   return 0
 fi
 
-SCALA_VERSION="2.10.3"
+SCALA_VERSION="2.11.7"
 
 if [[ "0.7.3 0.8.0 0.8.1" =~ $SPARK_VERSION ]]; then
   SCALA_VERSION="2.9.3"
 fi
 
 echo "Unpacking Scala"
-wget http://s3.amazonaws.com/spark-related-packages/scala-$SCALA_VERSION.tgz
+wget https://philm.brandwatch.s3.amazonaws.com/scala-$SCALA_VERSION.tgz
 tar xvzf scala-*.tgz > /tmp/spark-ec2_scala.log
 rm scala-*.tgz
 mv `ls -d scala-* | grep -v ec2` scala
